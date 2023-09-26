e3e86ca4dfb6523027b990530d3f5817b818f07a
Mafe <ferdlgpar@gmail.com>
2023-09-26 18:54:34 -0500

    configuracion de jwt

---
+++ back/api/Controllers/Auth/login.py
@@ -0,0 +1,6 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import 
+from rest_framework_jwt.settings import api_settings
