80eb90c399336537a128422e1efd310fd7336438
Mafe <ferdlgpar@gmail.com>
2023-09-27 20:03:18 -0500

    avance de autenticacion y login

--- back/api/Controllers/usuarias.py
+++ back/api/Controllers/usuarias.py
@@ -1,7 +1,34 @@
 from rest_framework import viewsets
 from ..models import Usuarias
 from .serializers import UsuariasSerializer
+from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt 
 class usuariasCRUD(viewsets.ModelViewSet):
     queryset = Usuarias.objects.all()
     serializer_class = UsuariasSerializer
+
+class EmailBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None,**kwargs):
+            UsuariaModel = get_user_model()
+            try:
+                usuaria = UsuariaModel.objects.get(email=username)
+            except UsuariaModel.DoesNotExist:
+                return None
+            else:
+                if usuaria.check_password(password):
+                    return usuaria
+            return None
+    def login(request):
+         email = request.POST.get('email')
+         password = request.POST.get('password')
+         usuaria = authenticate(request, email = email, password=password)
+
+         if usuaria:
+              return HttpResponse('Usuaria autenticada')
+         else:
+              return HttpResponse('Usuaria no autenticada')
